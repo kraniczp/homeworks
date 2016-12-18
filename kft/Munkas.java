9a162d69dca8d168123f9b08a6ad4b106650a683
kraniczp <kraniczpjotr@gmail.com>
2016-12-18 10:30:07 +0100

    Delete Munkas.java

--- kft/Munkas.java
+++
@@ -1,8 +0,0 @@
-package kft;
-
-public class Munkas extends Ember {
-    
-    public Munkas(String nev, String szul_datum, int vagyon, int ber){
-        super(nev, szul_datum, vagyon, ber);
-    }
-}
